c2c957ca47a5bafdbf36d71ccc6b4690fd7d0511
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-03-29 18:01:04 -0500

    pseudocode for JS and HTML routes

--- public/assets/js/webScraper.js
+++ public/assets/js/webScraper.js
@@ -1,1 +1,16 @@
 console.log("we are linked");
+
+//  do click event here 
+
+
+//  and then whatever else is in the process flow diagram 
+
+//  do the web scraping thing 
+
+//  put somewhere - similar to the class activities  - they store in empty array 
+
+//  can prolly pass the array to DB? 
+
+//  send to route to DB
+
+//  then there should be something in HBS that would assist with passing that
---
+++ routes/api-routes/api-routes.js
@@ -0,0 +1,11 @@
+//  web scraping routes go here 
+
+//  ROUTES FOR obtaining articles 
+//  get route 
+
+//  post route 
+
+//  routes for adding comments 
+
+
+//  then delete and update routes - that would be by id 

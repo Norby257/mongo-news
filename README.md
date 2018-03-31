940e67130d92472268714269e4fe71ea1b660911
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-03-30 23:43:53 -0500

    fixed typo in readme

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a full-stack web application that allows users to view news and leave co
 
 ##How to use 
 
-When the user clicks on the    `Get Artciles` button, we launch a web scraper to get that content for them 
+When the user clicks on the    `Get Articles` button, we launch a web scraper to get that content for them 
 
 ###This app was built leveraging the following technologies 
 * MongoDB
--- routes/api-routes/api-routes.js
+++ routes/api-routes/api-routes.js
@@ -1,66 +1,57 @@
-
-
-
-//  get route for scraping the web 
-
+//  get route for scraping the web - re watch the video 
 
 //  route for getting articles from DB
-app.get("/all", function(req, res){
-    db.news.find({}, function(error, data){
-        //  log any errors for debugging 
-        if (error) {
-            console.log(error);
-        } else {
-            // if no error, send json of data back to user 
-            //  I don't have data here yet 
-            res.json(data)
-        }
-    })
+app.get("/all", function(req, res) {
+  db.news.find({}, function(error, data) {
+    //  log any errors for debugging
+    if (error) {
+      console.log(error)
+    } else {
+      // if no error, send json of data back to user
+      //  I don't have data here yet
+      res.json(data)
+    }
+  })
 })
 
-//  post an article to the database 
+//  post an article to the database
 
-app.post("/save", function(req, res){
-    //  logging req.body for debugging purposes
-    console.log(req.body);
-    //  insert the article into the articles collection 
-    db.news.insert(req.body, function(error, saved){
-        //  log errors if they occur 
-        if (error) {
-            console.log(error);
-        }
-        else {
-            //  send it to browser
-            res.send(saved)
-        }
-    })
+app.post("/save", function(req, res) {
+  //  logging req.body for debugging purposes
+  console.log(req.body)
+  //  insert the article into the articles collection
+  db.news.insert(req.body, function(error, saved) {
+    //  log errors if they occur
+    if (error) {
+      console.log(error)
+    } else {
+      //  send it to browser
+      res.send(saved)
+    }
+  })
 })
 
+//  routes for adding comments
 
+// route for deleting comments
 
-//  routes for adding comments 
+//  route for deleting saved articles
 
-
-// route for deleting comments 
-
-
-//  route for deleting saved articles 
-
-app.get("/delete/:id", function(req, res){
-    db.news.remove( 
-        {
-            _id: mongojs.ObjectID(req.params.id)
-        },
-        function(error, removed) {
-            //  log error 
-            if (error) {
-                console.log(error);
-                res.send(error)
-            } else {
-                //  send response to browser if successful 
-                console.log(removed);
-                res.send(removed);
-            }
-        }
-    )
+app.get("/delete/:id", function(req, res) {
+  db.news.remove(
+    {
+      _id: mongojs.ObjectID(req.params.id)
+    },
+    function(error, removed) {
+      //  log error
+      if (error) {
+        console.log(error)
+        res.send(error)
+      } else {
+        //  send response to browser if successful
+        console.log(removed)
+        res.send(removed)
+      }
+    }
+  )
 })
